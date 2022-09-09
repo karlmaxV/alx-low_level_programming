285ae301f4e9780eb4428fdfeaac9b1e88ad56ee
root <karlmaxsonvicki@gmail.com>
2022-09-09 08:09:53 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - Entry point
- * Return: Alway 1 (Success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
 }
