cfaf843748d48acd7ec2e110c00229016867017b
root <karlmaxsonvicki@gmail.com>
2022-12-16 03:21:51 -0800

    Dynamic lib

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - function checks for alphabetic characters
+ * @c: character to be sorted
+ *
+ * Return: 1 if c is (lowercase or uppercase), 0 if otherwise
+ */
+int _isalpha(int c)
+{
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+		return (1);
+	else
+		return (0);
+}
