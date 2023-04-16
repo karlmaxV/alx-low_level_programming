72e8a8278b3a1ddc00e5b393270b03f7e53e4318
root <karlmaxsonvicki@gmail.com>
2023-04-16 11:06:05 -0700

    old new

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,76 +1,65 @@
 #include "search_algos.h"
 
 /**
-  * print_array - print array with limits in l and r
-  * @array: set of numbers
-  * @l: left limit
-  * @r: right limit
-  * Return: nothing
-  */
-void print_array(int *array, size_t l, size_t r)
-{
-	printf("Searching in array: ");
-	for (l = l; l < r; l++)
-		printf("%d, ", array[l]);
-	printf("%d\n", array[l]);
-}
-
-/**
-  * binary_search_p - search the first ocurrency of a value in the array
-  * @array: set of numbers
-  * @l: left limit
-  * @r: right limit
-  * @value: value to search
-  * Return: return the first index located otherwise -1
-  */
-int binary_search_p(int *array, size_t l, size_t r, int value)
-{
-	size_t mid = 0;
-
-	while (l <= r)
-	{
-		print_array(array, l, r);
-		mid = (l + r) / 2;
-		if (array[mid] < value)
-			l = mid + 1;
-		else if (array[mid] > value)
-			r = mid - 1;
-		else
-			return (mid);
-	}
-	return (-1);
-}
-
-/**
-  * min_value - get the min value of two numbers
-  * @l: number l
-  * @r: number r
-  * Return: return the minimun value
-  */
-size_t min_value(size_t l, size_t r)
-{
-	return ((l < r) ? l : r);
-}
-
-/**
-  * exponential_search - search the first ocurrency of a value in the array
-  * @array: set of numbers
-  * @size: size of the array
-  * @value: value to search
-  * Return: return the first index located otherwise -1
-  */
+ * exponential_search - performs exponential search
+ * @array: the integer array
+ * @size: its size
+ * @value: value to search for
+ *
+ * Return: the index found or -1
+ */
 int exponential_search(int *array, size_t size, int value)
 {
-	size_t pow = 1;
+	size_t i = 1, newsize = 0;
+	int ret;
 
-	if (array == NULL)
+	if (!array || !size)
 		return (-1);
-	while (pow < size && array[pow] < value)
+
+	while (i < size && array[i] < value)
 	{
-		printf("Value checked array[%d] = [%d]\n", (int)pow, array[pow]);
-		pow *= 2;
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		i <<= 1;
 	}
-	printf("Value found between indexes [%d] and [%d]\n",
-					(int)(pow / 2), (int)min_value(pow, size - 1));
-	return (binary_search_p(array, pow / 2, min_value(pow, size - 1), value));
+	newsize = (i >= size ? size : i + 1) - (i >> 1);
+	i >>= 1;
+	printf("Value found between indexes [%lu] and [%lu]\n",
+			i, i << 1 >= size ? size - 1 : i << 1);
+	ret = binary_search(array + i, newsize, value);
+	return (ret == -1 ? ret : ret + (int)i);
+}
+
+/**
+ * binary_search - performs binary search
+ * @array: the integer array
+ * @size: its size
+ * @value: value to search for
+ *
+ * Return: the index found or -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+	int *a = array;
+
+	if (!array)
+		return (-1);
+
+	while (size)
+	{
+		for (i = 0, printf("Searching in array: "); i < size; i++)
+			printf("%d%s", a[i], i + 1 == size ? "\n" : ", ");
+
+		i = (size - 1) / 2;
+		if (a[i] == value)
+			return ((a - array) + i);
+		else if (a[i] > value)
+			size = i;
+		else
+		{
+			a += (i + 1);
+			size -= (i + 1);
+		}
+	}
+	return (-1);
 }
